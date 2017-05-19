34821bbdb9ffdf67ca99aab7e6c57b4dc83650bd
haziqn <hazinqhar20@gmail.com>
2017-05-20 04:07:41 +0800

    sign up is almost working

---
+++ app/src/main/java/sg/edu/rp/c346/eventful_organiser/SignIn.java
@@ -0,0 +1,13 @@
+package sg.edu.rp.c346.eventful_organiser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
