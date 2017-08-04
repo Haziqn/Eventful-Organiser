6f3bee341c20106ced60323fbb7afa1bc23d2403
15017470 <15017470@myrp.edu.sg>
2017-08-04 16:32:07 +0800

    Custom dialog for terms and conditios upo registering an account implemented.

--- app/src/main/java/sg/edu/rp/c346/eventful_organiser/SignUp.java
+++ app/src/main/java/sg/edu/rp/c346/eventful_organiser/SignUp.java
@@ -1,6 +1,8 @@
 package sg.edu.rp.c346.eventful_organiser;
 
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
@@ -9,11 +11,13 @@ import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Base64;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -21,6 +25,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Toast;
 
@@ -189,8 +194,31 @@ public class SignUp extends AppCompatActivity {
                 password = editTextPassword.getText().toString().trim();
                 password2 = editTextConfirmPassword.getText().toString().trim();
                 if (field_verification(name, address, description, website, acra, contact_number, type, email, password, password2, uri)) {
-                    mProgress.hide();
-                    startRegister();
+
+                    LayoutInflater inflater = (LayoutInflater)
+                            getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    LinearLayout dialog =
+                            (LinearLayout) inflater.inflate(R.layout.termsandconditions, null);
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(SignUp.this);
+                    builder.setTitle("Terms and Conditions")
+                            .setView(dialog)
+                            .setNegativeButton("I disagree", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            })
+                            .setPositiveButton("I agree", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int id) {
+                                    finish();
+                                    mProgress.show();
+                                    startRegister();
+                                }
+                            });
+                    AlertDialog alertDialog = builder.create();
+                    alertDialog.show();
                 } else if (uri == null) {
                     Toast.makeText(SignUp.this, "Please select an image", Toast.LENGTH_SHORT).show();
                 } else {
